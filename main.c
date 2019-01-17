e84255f90e3ac5bd9dc69fc6eb961943baea64e4
unknown <627741487@qq.com>
2019-01-17 09:12:27 +0800

    添加敏感字符替换功能

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 char arr[1024];
 int index = 0;
+int isMingan(char x){
+    int count = 0;
+    for(int i=0;i<index;i++){
+        if(x == arr[i]){
+            count++;
+        }
+    }
+
+    if(count == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 
 int main(){
 
@@ -49,14 +63,31 @@ int main(){
             for(int i =0; i< index; i++){
                 printf("第%d个敏感字符为%c\n", i+1,arr[i]);
             }
-            
+
             printf("点击回车继续\n");
             char x;
             scanf("%c", &x);
             scanf("%c", &x);
         }
         if(code == 4){
-            
+            printf("请输入一段英文:\n");
+            char neirong[1024];
+            scanf("%s",neirong);
+            for(int i = 0;1;i++){
+                if(neirong[i] == '\0'){
+                    break;
+                }
+                if(isMingan(arr[i]) == 1){
+                    neirong[i] = '*';
+                }
+                printf("%c",neirong[i]);
+            }
+            printf("\n");
+
+            printf("替换完成，点击回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
         if(code == 5){
             printf("退出系统\n");
